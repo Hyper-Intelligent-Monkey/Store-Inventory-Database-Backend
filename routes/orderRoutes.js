97c726815dbc7c59aa0f9aa98e8ab2a63684c1ab
Hyper-Intelligent-Monkey <goosechooseyou@gmail.com>
2025-12-03 22:23:08 +0800

    route for my-pending

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const auth = require('../middleware/authMiddleware');
 
 const orderController = require('../controllers/orderController');
-
+router.get('/orders/my-pending', auth, orderController.getMyPendingOrder);//
 router.get('/orders', auth, orderController.getAllOrders);
 router.get('/order/:id', auth, orderController.getOrder);
 router.post('/order', auth, orderController.createOrder);
