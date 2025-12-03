0b32ca2152e846123affd74cb79b6b293edf8ee8
Hyper-Intelligent-Monkey <goosechooseyou@gmail.com>
2025-12-03 19:48:32 +0800

    get the supplier email from productController

--- controllers/productController.js
+++ controllers/productController.js
@@ -14,7 +14,7 @@ exports.getMyProducts = async (req, res) => {
 // Get All Products
 exports.getAllProducts = async (req, res) => {
   try {
-    const products = await Product.find();
+    const products = await Product.find().populate('supplier', 'email');
     res.json(products);
   } catch (err) {
     res.status(500).json({ message: err.message });
